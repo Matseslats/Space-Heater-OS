da8a89367a53aefb133215dd26abeb4f8d06f2bf
Mats <49249061+Matseslats@users.noreply.github.com>
2023-10-30 20:30:59 +0000

    Optimized printing

--- boot.asm
+++ boot.asm
@@ -1,4 +1,21 @@
+[org 0x7c00]
+mov ah, 0x0e
+mov bx, outputString
+
+printString:
+    mov al, [bx]
+    cmp al, 0
+    je foreverLoop
+    int 0x10
+    inc bx
+    jmp printString
+
+foreverLoop:
+
 jmp $
+
+outputString:
+    db "Space Heater OS", 0
 
 times 510-($-$$) db 0
 db 0x55, 0xaa
